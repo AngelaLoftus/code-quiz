3d70793a1c0ded94928bce8ec30c2aacb46b0aa8
Angela Loftus <angelaloftus@yahoo.com>
2021-09-04 12:09:57 -0500

    starting point

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,101 @@
-//present a question
-//which question/index are we on?
-//access that array using the index we are currently on
-//acces the object question and choices
-//and set the corresponding html elements innertext to the objects values
+var questions = [
+    //that's why we want to put the question in the object so that it's all stored in one spot in the array 
+    {   //index of 0 
+        //key/value pair the question is the key, the value is question 1
+        question: "question 1",
+        option1: "q1 o1",
+        option2: "q1 o2",
+        option3: "q1 o3",
+        option4: "q1 o4",
+        correctAnswer: "q1 o2"
+    },
+    { 
+        //index 1
+        question: "question 2",
+        option1: "q2 o1",
+        option2: "q2 o2",
+        option3: "q2 o3",
+        option4: "q2 o4",
+        correctAnswer: "q2 o2"
+    },
 
-//WHEN i answer a question
-//grab the choice buttons
+    { //index 2
+    question: "question 3",
+        option1: "q3 o1",
+        option2: "q3 o2",
+        option3: "q3 o3",
+        option4: "q3 o4",
+        correctAnswer: "q1 o2"
+
+    }
+]
+//index starts at 0 
+var questionNumber = 0; 
+var time = 75;
+
+//put this inside the setInterval
+//time--
+// document.getElementById("timer").innerText = time
+
+
+
+nextQuestion(); //depends on that variable existing 
+
+//put quotes unless you're passing a variable or a function
+
+//4 seperate event listeners (will only execute questionNumber++ once)
+document.getElementById("option1").addEventListener("click", function(){
+
+    //MORE LOGIC HERE
+    questionNumber++
+    nextQuestion();
+})
+
+document.getElementById("option2").addEventListener("click", function(){
+
+    questionNumber++
+    nextQuestion();
+})
+
+document.getElementById("option3").addEventListener("click", function(){
+
+    questionNumber++
+    nextQuestion();
+})
+
+document.getElementById("option4").addEventListener("click", function(){
+
+    questionNumber++
+    nextQuestion();
+})
+
+
+//every time you click the start button, it's gonna increase the index by 1, and show the text for the new questions prompt 
+function nextQuestion () {
+    
+    document.getElementById("question").innerHTML = questions[questionNumber].question 
+    document.getElementById("option1").innerHTML = questions[questionNumber].option1
+    document.getElementById("option2").innerHTML = questions[questionNumber].option2
+    document.getElementById("option3").innerHTML = questions[questionNumber].option3
+    document.getElementById("option4").innerHTML = questions[questionNumber].option4
+
+}
+
+//setInterval for timer
+// change the timer in the HTML (getElementById("timer").innerHTML = )
+
+//check answer function
+//it decrements by 5 if they're wrong 
+//if else conditional statement if they're right, do blah, if they're wrong, take off time
+
+
+//save multiple initials, name initial object to an array 
+
+
+//local storage, get user input for initials after quiz is over 
+//make sure to call clear interval when the test is over 
+//if have 4 questions, when questionNumber = 4, then clear interVal (when it hits the max number of questions)
+
+
+
+//on topic, close enough
