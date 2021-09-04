3d17e911d769e2d582adf7847ac35a02d57f717e
Angela Loftus <angelaloftus@yahoo.com>
2021-09-04 13:31:50 -0500

    timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@ var questions = [
     //that's why we want to put the question in the object so that it's all stored in one spot in the array 
     {   //index of 0 
         //key/value pair the question is the key, the value is question 1
-        question: "question 1",
-        option1: "q1 o1",
-        option2: "q1 o2",
-        option3: "q1 o3",
-        option4: "q1 o4",
-        correctAnswer: "q1 o2"
+        question: "Which method allows you to set the item to local storage?",
+        option1: "localStorage.setItem()",
+        option2: "localStorage.getItem()",
+        option3: "localStorage.saveItem()",
+        option4: "localStorage.storeItem()",
+        correctAnswer: "localStorage.setItem()"
     },
     { 
         //index 1
@@ -58,15 +58,30 @@ var questions = [
 ]
 //index starts at 0 
 var questionNumber = 0; 
-var time = 75;
+var timeLeft = 15;
 
 //put this inside the setInterval
 //time--
-document.getElementById("timer").innerText = time
 
+
+function startTimer() {
+    
+        var quizTimer = setInterval(function() {
+            if (timeLeft <=0) {
+                clearInterval(quizTimer);
+                document.getElementById("timer").innerText = "Sorry, you have run out of time.";
+                alert("You ran out of time!");
+            }
+            timeLeft--;
+            console.log(timeLeft + " seconds left");
+            document.getElementById("timer").innerText = timeLeft;
+        }, 1000);
+}
 document.getElementById("start").addEventListener("click", function() {
     nextQuestion();
+    startTimer()
 })
+
 
  //depends on that variable existing 
 
@@ -111,6 +126,12 @@ function nextQuestion () {
 }
 
 
+//ask user for name and save to local storage
+
+if (questionNumber>=6) {
+var userName = prompt("What are your initials?");
+
+localStorage.setItem("name", userName); };
 //setInterval for timer
 // change the timer in the HTML (getElementById("timer").innerHTML = )
 
