dfb2e75163a119d9b51fc475c457565f1f6deeb0
Angela Loftus <angelaloftus@yahoo.com>
2021-09-04 11:00:41 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//present a question
+//which question/index are we on?
+//access that array using the index we are currently on
+//acces the object question and choices
+//and set the corresponding html elements innertext to the objects values
+
+//WHEN i answer a question
+//grab the choice buttons
